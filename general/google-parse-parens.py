5ded10afde7c882600459203ea8f624f91d9327f
Shawn Cicoria <github@cicoria.com>
2021-06-13 19:09:48 -0400

    add omment

--- general/google-parse-parens.py
+++ general/google-parse-parens.py
@@ -1,3 +1,10 @@
+# Input (string with only well balanced parenthesis and spaces are only for illustrative purposes):
+# ( ( ) ( ) ) ( ) ( )
+# 0 1 2 3 4 5 6 7 8 9
+#
+# Output:
+# [(0, 5), (1, 2), (3, 4), (6, 7), (8, 9)]
+
 from typing import List
 
 def parse(items: List):
