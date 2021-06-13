1a2d722da8206f88d1098976d3037110de45dd66
Shawn Cicoria <github@cicoria.com>
2021-06-13 18:34:50 -0400

    c++ version

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+// #include <algorithm>
 
-int MaxPairwiseProduct(const std::vector<int>& numbers) {
-    int max_product = 0;
-    int n = numbers.size();
+typedef long long int int64;
 
-    for (int first = 0; first < n; ++first) {
-        for (int second = first + 1; second < n; ++second) {
+int64 MaxPairwiseProduct(const std::vector<int64_t>& numbers) {
+    int64_t max_product = 0;
+    int64_t n = numbers.size();
+
+    for (int64_t first = 0; first < n; ++first) {
+        for (int64_t second = first + 1; second < n; ++second) {
             max_product = std::max(max_product,
                 numbers[first] * numbers[second]);
         }
@@ -17,14 +19,14 @@ int MaxPairwiseProduct(const std::vector<int>& numbers) {
 }
 
 int main() {
-    int n;
+    int64_t n;
     std::cin >> n;
-    std::vector<int> numbers(n);
-    for (int i = 0; i < n; ++i) {
+    std::vector<int64_t> numbers(n);
+    for (int64_t i = 0; i < n; ++i) {
         std::cin >> numbers[i];
     }
 
-    std::cout << n << "\n";
+    std::cout << "using 64" << sizeof(int64_t) << "\n";
     std::cout << MaxPairwiseProduct(numbers) << "\n";
     return 0;
 }
